783f90957829b71281e114a8bbdf982b51fe4e0f
Koce Georgiev <koce@mentalchallenge.tk>
2020-04-27 22:46:48 +0200

    adding Filename function

--- par2/file.go
+++ par2/file.go
@@ -11,6 +11,10 @@ func (f *File) ID() string {
 	return fmt.Sprintf("%x", f.FileDescPacket.FileID)
 }
 
+func (f *File) Filename() string {
+	return f.FileDescPacket.Filename
+}
+
 func (f *File) Valid() bool {
 	return f.FileDescPacket != nil && f.IFSCPacket != nil
 }
